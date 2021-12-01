cc0c691517f227004e07da8306d67d635091905b
Peter Gorgone <947820+peltronic@users.noreply.github.com>
2021-11-30 17:17:30 -0800

    Add Dockerfile

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require("http")
 const app = require("./app")
 const server = http.createServer(app)
 
-const { API_PORT } = process.env
-const port = process.env.PORT || API_PORT
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`)
